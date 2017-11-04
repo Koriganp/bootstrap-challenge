0f58b208a7845c889e4d70922d6caf132f74ab6b
Korigan Payne <koriganp@gmail.com>
2017-11-04 16:37:14 -0600

    added jumbotron and navbar

--- index.php
+++ index.php
@@ -13,6 +13,28 @@
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
-
+		<nav class="nav justify-content-end">
+			<a class="nav-item nav-link active">Title</a>
+			<a class="nav-item nav-link" href="#">Link1</a>
+			<a class="nav-item nav-link" href="#">Link2</a>
+			<a class="nav-item nav-link" href="#">Link3</a>
+			<a class="nav-item nav-link" href="#">Link4</a>
+		</nav>
+		<header class="info">
+			<div class="jumbotron">
+			<h1 class="display-4">Hello, world!</h1>
+			<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+			<button class="btn btn-primary btn-lg">Learn more</button>
+			</div>
+		</header>
+		<div class="container">
+			<div>
+				<img src="images/rattlesnake.jpg" alt="Image of a rattlesnake"/>
+			</div>
+			<div>
+				<p>You can do anything your heart can imagine. Trees grow however makes them happy. Put your feelings into it, your heart, it's your world. I thought today we would make a happy little stream that's just running through the woods here. Only eight colors that you need. Anytime you learn something your time and energy are not wasted. Isn't that fantastic that you can create an almighty tree that fast?</p>
+				<p>In your world you have total and absolute power. That's a son of a gun of a cloud. Nice little clouds playing around in the sky. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Paint anything you want on the canvas. Create your own world. We want to use a lot pressure while using no pressure at all.</p>
+			</div>
+		</div>
 	</body>
 </html>
