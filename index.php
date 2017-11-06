309a964cd70516a2aa33a38cf1f62daba9194ce3
Korigan Payne <koriganp@gmail.com>
2017-11-06 14:48:55 -0700

    made it all bootstrap AND it still works!!

--- index.php
+++ index.php
@@ -6,14 +6,13 @@
 		<meta name="description" content="Bootstrap Challenge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
-		<link rel="stylesheet" href="styles/style.css" type="text/css"/>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
-		<nav class="navbar navbar-default navbar-expand-md navbar-light bg-light text-secondary rounded border border-dark" role="navigation">
+		<nav class="navbar navbar-default navbar-expand-md navbar-light bg-light text-secondary rounded border border-dark m-4" role="navigation">
 			<a class="navbar-brand" href="#">Title</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -35,23 +34,22 @@
 				</ul>
 			</div>
 		</nav>
-		<header class="title">
+		<header class="m-4">
 			<div class="jumbotron">
-				<h1 class="display-3">Hello, world!</h1>
-				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for
-					calling extra attention to featured content or information.</p>
+				<h1 class="display-3">No pressure.</h1>
+				<p class="lead">Just pretend you are a whisper floating across a mountain. There isn't a rule. You just practice and find out which way works best for you. Now, we're going to fluff this cloud. Only think about one thing at a time. Don't get greedy.</p>
 				<p class="lead">
-					<a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+					<a class="btn btn-primary btn-lg" href="#" role="button">Paint</a>
 				</p>
 			</div>
 		</header>
-		<section class="info">
+		<section class="bg-secondary p-5">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-3">
-						<img src="images/rattlesnake.jpg" alt="Image of a rattlesnake"/>
+					<div class="col-md-6 col-lg-3">
+						<img class="mw-100 mt-4" src="images/rattlesnake.jpg" alt="Image of a rattlesnake"/>
 					</div>
-					<div class="col-md-9">
+					<div class="col-md-6 col-lg-9">
 						<p>You can do anything your heart can imagine. Trees grow however makes them happy. Put your feelings into it, your heart, it's your world. I thought today we would make a happy little stream that's just running through the woods here. Only eight colors that you need. Anytime you learn something your time and energy are not wasted. Isn't that fantastic that you can create an almighty tree that fast?</p>
 						<p>In your world you have total and absolute power. That's a son of a gun of a cloud. Nice little clouds playing around in the sky. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Paint anything you want on the canvas. Create your own world. We want to use a lot pressure while using no pressure at all.</p>
 					</div>
@@ -61,33 +59,33 @@
 		<section>
 			<div class="container">
 				<div class="row">
-					<div class="card-deck">
-						<div class="col-md-3">
-							<div class="card border-0">
+					<div class="card-deck mt-4">
+						<div class="col-md-6 col-lg-3">
+							<div class="card border-0 m-3">
 								<img class="card-img-top rounded-circle mw-100" src="images/bob.png" alt="Image of Bob">
 								<div class="card-body">
 									<p class="card-text border rounded p-2">Every day I learn. Everything is happy if you choose to make it that way. That's a son of a gun of a cloud. There we go.</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-3">
-							<div class="card border-0">
+						<div class="col-md-6 col-lg-3">
+							<div class="card border-0 m-3">
 								<img class="card-img-top rounded-circle mw-100" src="images/bobby.png" alt="Image of Bobby">
 								<div class="card-body">
 									<p class="card-text border rounded p-2">You have to make those little noises or it won't work. That's the way I look when I get home late; black and blue.</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-3">
-							<div class="card border-0">
+						<div class="col-md-6 col-lg-3">
+							<div class="card border-0 m-3">
 								<img class="card-img-top rounded-circle mw-100" src="images/robert.png" alt="Image of Robert">
 								<div class="card-body">
 									<p class="card-text border rounded p-2">Nothing wrong with washing your brush. La- da- da- da- dah. Just be happy. Imagination is the key to painting.</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-3">
-							<div class="card border-0">
+						<div class="col-md-6 col-lg-3">
+							<div class="card border-0 m-3">
 								<img class="card-img-top rounded-circle mw-100" src="images/bobbette.png" alt="Image of Bobbette">
 								<div class="card-body">
 									<p class="card-text border rounded p-2">Just go back and put one little more happy tree in there. We don't have to be concerned about it. We just have to let it fall where it will.</p>
@@ -98,7 +96,7 @@
 				</div>
 			</div>
 		</section>
-		<footer class="footer bg-dark text-light text-center">
+		<footer class="p-4 bg-dark text-light text-center">
 			<p>You're meant to have fun in life.</p>
 		</footer>
 	</body>
